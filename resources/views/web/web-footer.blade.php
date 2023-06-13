5023a123d3fbb3dbed801ac6162b3a14f54d00cc
abdulrehman24 <ar5555789@gmail.com>
2023-06-13 10:40:36 +0500

    FB chat

--- resources/views/web/web-footer.blade.php
+++ resources/views/web/web-footer.blade.php
@@ -352,34 +352,7 @@
 <div id="fb-customer-chat" class="fb-customerchat">
 </div>
 
-<script>
-    function informationflag($text){
-        localStorage.setItem("information", $text);
-    }
 
-    // var chatbox = document.getElementById('fb-customer-chat');
-    // chatbox.setAttribute("page_id", "158773151449430");
-    // chatbox.setAttribute("attribution", "biz_inbox");
-</script>
-
-<!-- Your SDK code -->
-{{-- <script>
-    window.fbAsyncInit = function() {
-        FB.init({
-            xfbml            : true,
-            version          : 'v16.0'
-        });
-    };
-
-    (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        js.style.zIndex = "-1";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-</script> --}}
 
 
 <script src="{{ url('/assets-web') }}/vendors/jquery.min.js"></script>
@@ -1127,6 +1100,34 @@
         $("#menuicon_routine").toggleClass('fa-plus', 500);
      }
 
+</script>
+<script>
+    function informationflag($text){
+        localStorage.setItem("information", $text);
+    }
+
+    var chatbox = document.getElementById('fb-customer-chat');
+    chatbox.setAttribute("page_id", "158773151449430");
+    chatbox.setAttribute("attribution", "biz_inbox");
+</script>
+
+<!-- Your SDK code -->
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            xfbml            : true,
+            version          : 'v16.0'
+        });
+    };
+
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        js.style.zIndex = "-1";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
 </script>
 
 </html>
